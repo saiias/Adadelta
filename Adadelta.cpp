a5465506779e11a6717c41cd6cbaca452eff5d35
saiias <saiias0708@gmail.com>
2015-01-03 14:14:48 +0900

    refactoring

--- Adadelta.cpp
+++ Adadelta.cpp
@@ -1,149 +1,74 @@
-#include<iostream>
-#include<Eigen/Dense>
-#include<cmath>
-#include<vector>
-#include<sstream>
-#include<fstream>
-#include<iostream>
-#include<string>
+#include <Eigen/Dense>
+#include <cmath>
+#include <vector>
+#include "Adadelta.hpp"
 
-#define DEBUG(x) cout<<"line"<<__LINE__<<":"<<#x" == "<<x<<endl
 #define EIGEN_MPL2_ONLY
 using namespace Eigen;
 using namespace std;
 
-class Adadelta{
-public:
-  double C;
-  double rho;
-  double eps;
-  MatrixXd X;
-  VectorXd label;
-  VectorXd w;
-  VectorXd E;
-  VectorXd Edx;
-  int iteration;
-  int N;
-  int d;
-  vector<double> iterscores;
-  Adadelta(int _N,int _d,MatrixXd _x,VectorXd _label,double _C,double _rho,double _eps,int iter){
-    N =_N;
-    d = _d;
-    X = _x;
-    label = _label;
-    C = _C;
-    rho = _rho;
-    eps = _eps;
-    w = VectorXd::Random(d);
-    E = VectorXd::Zero(d);
-    Edx = VectorXd::Zero(d);
-    iteration = iter;
-  }
+Adadelta::Adadelta(int _N,int _d,MatrixXd _x,VectorXd _label,double _C,double _rho,double _eps,int iter):
+  N(_N),
+  d(_d),
+  C(_C),
+  rho(_rho),
+  eps(_eps),
+  X(_x),
+  label(_label),
+  E(VectorXd::Zero(d)),
+  Edx(VectorXd::Zero(d)),
+  iteration(iter),
+  w(VectorXd::Random(d))
+{}
 
-  double Acc(vector<double>& pred,VectorXd &l){
-    int t =0;
-    double loss =0;
-    for(int i = 0; i< pred.size();i++){
-      loss += (l(i) - pred[i]) * (l(i) - pred[i]);
-      int s = pred[i] > 0.5 ? 1 : 0;
-      if(s == l(i)){
-        t++;
-      }
+double Adadelta::Acc(vector<double>& pred,VectorXd &l){
+  int t =0;
+  double loss =0;
+  for(int i = 0; i< pred.size();i++){
+    loss += (l(i) - pred[i]) * (l(i) - pred[i]);
+    int s = pred[i] > 0.5 ? 1 : 0;
+    if(s == l(i)){
+      t++;
     }
-    cout<<"Accuracy: "<<(double)t/pred.size()<<endl;
-    // cout<<"Error: "<<loss<<endl;
-    return (double)t/pred.size();
   }
-  double rms(double z){
-    return sqrt(z + eps);
-  }
+  return (double)t/pred.size();
+}
 
-  void train(){
-    for(int iter = 0; iter< iteration; iter++){
-      cout<<"Iteration:"<<iter<<endl;
-      for(int i = 0; i < N; i++){
-        double pred = sigma(X,i);
-        for(int idx = 0; idx < d; idx++){
-          if(X(i,idx) != 0){
-            double grad = (pred - label(i)) * X(i,idx) + C * w(idx);
-            E[idx] = rho * E[idx] + (1.0 - rho) * grad *grad;
-            double deltax = -(rms(Edx(idx))/rms(E(idx))) * grad;
-            Edx[idx] = rho * Edx[idx] + (1.0 - rho) * deltax * deltax;
-            w(idx) += deltax;
-          }
+double Adadelta::rms(double z){
+  return sqrt(z + eps);
+}
+
+void Adadelta::train(){
+  for(int iter = 0; iter< iteration; iter++){
+    for(int i = 0; i < N; i++){
+      double pred = sigma(X,i);
+      for(int idx = 0; idx < d; idx++){
+        if(X(i,idx) != 0){
+          double grad = (pred - label(i)) * X(i,idx) + C * w(idx);
+          E[idx] = rho * E[idx] + (1.0 - rho) * grad *grad;
+          double deltax = -(rms(Edx(idx))/rms(E(idx))) * grad;
+          Edx[idx] = rho * Edx[idx] + (1.0 - rho) * deltax * deltax;
+          w(idx) += deltax;
         }
       }
-      vector<double> ret;
-      predict(X,label,ret);
-      iterscores.push_back(Acc(ret,label));
     }
-  }
-
-  double sigma(const MatrixXd& _x, int i){
-    return sigmoid(_x.row(i).dot(w));
-  }
-
-  double sigmoid(double z){
-    return 1.0 / (1.0 + exp(-z));
-  }
-
-  void predict(MatrixXd& _x,VectorXd& _l,vector<double>& ret){
-    for(int i = 0; i < _x.rows(); i++){
-      ret.push_back(sigma(_x,i));
-    }
-  }
-};
-
-void read(string filepath, MatrixXd& x,VectorXd& l){
-  ifstream ifs(filepath);
-  if(ifs.fail()){
-    cerr << "File do not exist."<<endl;
-    exit(0);
-  }
-  string s;
-  int index = 0;
-  while(getline(ifs,s)){
-    stringstream ss(s);
-    int t;
-    ss>>t;
-    if(t>0){
-      l(index) = 1;
-    }else{
-      l(index) = 0;
-    }
-    string feature;
-    while(ss>>feature){
-      string::size_type idx = feature.find(":");
-      int n = atoi(feature.substr(0,idx).c_str());
-      double value = atof(feature.substr(idx+1).c_str());
-      x(index,n-1) = value;
-    }
-    ++index;
+    vector<double> ret;
+    predict(X,label,ret);
+    iterscores.push_back(Acc(ret,label));
   }
 }
 
-int main()
-{
-  MatrixXd x = MatrixXd::Zero(32561,123);
-  VectorXd label(32561);
-  MatrixXd x_test = MatrixXd::Zero(16281,123);
-  VectorXd label_test(16281);
-  read("a9a",x,label);
-  read("a9a.t",x_test,label_test);
+double Adadelta::sigma(const MatrixXd& _x, int i){
+  return sigmoid(_x.row(i).dot(w));
+}
 
-  Adadelta lr = Adadelta(x.rows(),x.cols(),x,label,0.01,0.95,0.0000001,300);
-  lr.train();
-  vector<double> scores;
-  lr.predict(x_test,label_test,scores);
-  cout <<"Accuracy:"<< lr.Acc(scores,label_test) <<endl;
-  
-  ofstream iofs("adadelta_iters.txt");
-  for(double d : lr.iterscores){
-    iofs<<d<<endl;
-  }
+double Adadelta::sigmoid(double z){
+  return 1.0 / (1.0 + exp(-z));
+}
 
-  ofstream wofs("adadelta_weight.txt");
-  for(int i = 0; i< lr.w.rows();i++){
-    wofs<<lr.w(i)<<endl;
+void Adadelta::predict(MatrixXd& _x,VectorXd& _l,vector<double>& ret){
+  for(int i = 0; i < _x.rows(); i++){
+    ret.push_back(sigma(_x,i));
   }
 }
+
--- Adagrad.cpp
+++ Adagrad.cpp
@@ -1,141 +1,65 @@
-#include<iostream>
-#include<Eigen/Dense>
-#include<cmath>
-#include<vector>
-#include<sstream>
-#include<fstream>
-#include<iostream>
-#include<string>
+#include <Eigen/Dense>
+#include <cmath>
+#include <vector>
+#include "Adagrad.hpp"
 
-#define DEBUG(x) cout<<"line"<<__LINE__<<":"<<#x" == "<<x<<endl
 #define EIGEN_MPL2_ONLY
 using namespace Eigen;
 using namespace std;
 
-class Adagrad{
-public:
-  double C;
-  double eta;
-  MatrixXd X;
-  VectorXd label;
-  VectorXd w;
-  VectorXd E;
-  int iteration;
-  int N;
-  int d;
-  vector<double> iterscores;
-  Adagrad(int _N,int _d,MatrixXd _x,VectorXd _label,double _C,double _eta,int iter){
-    N =_N;
-    d = _d;
-    X = _x;
-    label = _label;
-    C = _C;
-    eta = _eta;
-    w = VectorXd::Random(d);
-    E = VectorXd::Zero(d);
-    iteration = iter;
-  }
+double Adagrad::sigma(const MatrixXd& _x, int i){
+  return sigmoid(_x.row(i).dot(w));
+}
 
-  double Acc(vector<double>& pred,VectorXd &l){
-    int t =0;
-    double loss =0;
-    for(int i = 0; i< pred.size();i++){
-      loss += (l(i) - pred[i]) * (l(i) - pred[i]);
-      int s = pred[i] > 0.5 ? 1 : 0;
-      if(s == l(i)){
-        t++;
-      }
+double Adagrad::sigmoid(double z){
+  return 1.0 / (1.0 + exp(-z));
+}
+
+Adagrad::Adagrad(int _N,int _d,MatrixXd _x,VectorXd _label,double _C,double _eta,int iter):
+  N(_N),
+  d(_d),
+  X(_x),
+  label(_label),
+  C(_C),
+  eta(_eta),
+  iteration(iter),
+  E(VectorXd::Zero(d)),
+  w(VectorXd::Random(d))
+{}
+
+double Adagrad::Acc(vector<double>& pred,VectorXd &l){
+  int t =0;
+  double loss =0;
+  for(int i = 0; i< pred.size();i++){
+    loss += (l(i) - pred[i]) * (l(i) - pred[i]);
+    int s = pred[i] > 0.5 ? 1 : 0;
+    if(s == l(i)){
+      t++;
     }
-    cout<<"Accuracy: "<<(double)t/pred.size()<<endl;
-    // cout<<"Error: "<<loss<<endl;
-    return (double)t/pred.size();
   }
+  return (double)t/pred.size();
+}
 
-  void train(){
-    for(int iter = 0; iter< iteration; iter++){
-      cout<<"Iteration:"<<iter<<endl;
-      for(int i = 0; i < N; i++){
-        double pred = sigma(X,i);
-        for(int idx = 0; idx < d; idx++){
-          if(X(i,idx) != 0){
-            double grad = (pred - label(i)) * X(i,idx) + C * w(idx);
-            E(idx) +=grad * grad;
-            w(idx) -= (eta/sqrt(E(idx)))*grad;
-          }
+void Adagrad::train(){
+  for(int iter = 0; iter< iteration; iter++){
+    for(int i = 0; i < N; i++){
+      double pred = sigma(X,i);
+      for(int idx = 0; idx < d; idx++){
+        if(X(i,idx) != 0){
+          double grad = (pred - label(i)) * X(i,idx) + C * w(idx);
+          E(idx) +=grad * grad;
+          w(idx) -= (eta/sqrt(E(idx)))*grad;
         }
       }
-      vector<double> ret;
-      predict(X,label,ret);
-      iterscores.push_back(Acc(ret,label));
-      // eta *= 0.5;
     }
+    vector<double> ret;
+    predict(X,label,ret);
+    iterscores.push_back(Acc(ret,label));
   }
-
-  double sigma(const MatrixXd& _x, int i){
-    return sigmoid(_x.row(i).dot(w));
-  }
-
-  double sigmoid(double z){
-    return 1.0 / (1.0 + exp(-z));
-  }
-
-  void predict(MatrixXd& _x,VectorXd& _l,vector<double>& ret){
-    for(int i = 0; i < _x.rows(); i++){
-      ret.push_back(sigma(_x,i));
-    }
-  }
-};
-
-void read(string filepath, MatrixXd& x,VectorXd& l){
-  ifstream ifs(filepath);
-  if(ifs.fail()){
-    cerr << "File do not exist."<<endl;
-    exit(0);
-  }
-  string s;
-  int index = 0;
-  while(getline(ifs,s)){
-    stringstream ss(s);
-    int t;
-    ss>>t;
-    if(t>0){
-      l(index) = 1;
-    }else{
-      l(index) = 0;
-    }
-    string feature;
-    while(ss>>feature){
-      string::size_type idx = feature.find(":");
-      int n = atoi(feature.substr(0,idx).c_str());
-      double value = atof(feature.substr(idx+1).c_str());
-      x(index,n-1) = value;
-    }
-    ++index;
+}
+void Adagrad::predict(MatrixXd& _x,VectorXd& _l,vector<double>& ret){
+  for(int i = 0; i < _x.rows(); i++){
+    ret.push_back(sigma(_x,i));
   }
 }
 
-int main()
-{
-  MatrixXd x = MatrixXd::Zero(32561,123);
-  VectorXd label(32561);
-  MatrixXd x_test = MatrixXd::Zero(16281,123);
-  VectorXd label_test(16281);
-  read("a9a",x,label);
-  read("a9a.t",x_test,label_test);
-
-  Adagrad lr = Adagrad(x.rows(),x.cols(),x,label,0.01,0.1,300);
-  lr.train();
-  vector<double> scores;
-  lr.predict(x_test,label_test,scores);
-  cout <<"Accuracy:"<< lr.Acc(scores,label_test) <<endl;
-  
-  ofstream iofs("adagrad_iters.txt");
-  for(double d : lr.iterscores){
-    iofs<<d<<endl;
-  }
-
-  ofstream wofs("adagrad_weight.txt");
-  for(int i = 0; i< lr.w.rows();i++){
-    wofs<<lr.w(i)<<endl;
-  }
-}
